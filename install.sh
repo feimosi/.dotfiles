74bc992adfc29d542d9cd1343afe4459af8e6629
Marek Grzybek <feimosi@users.noreply.github.com>
2018-09-16 16:41:50 +0200

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,3 @@
+sudo pacman -S pantheon-terminal keepass yaourt gparted chrome-gnome-shell nodejs tmux zsh yarn vim neofetch diff-so-fancy thunderbird fasd
+
+sudo yaourt -S google-chrome-dev visual-studio-code-bin slack-desktop nvm heroku-cli nerd-fonts-complete
