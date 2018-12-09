b6a61e63f65f7296b4bbdc28dee5dfbf706c869e
Marek Grzybek <feimosi@users.noreply.github.com>
2018-12-09 19:48:59 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,3 +1,3 @@
-sudo pacman -S pantheon-terminal keepass yaourt gparted chrome-gnome-shell nodejs tmux zsh yarn vim neofetch diff-so-fancy thunderbird fasd
+sudo pacman -S pantheon-terminal keepass yaourt gparted chrome-gnome-shell nodejs tmux zsh yarn vim neofetch diff-so-fancy thunderbird fasd docker moneymanagerex pinta peek
 
-sudo yaourt -S google-chrome-dev visual-studio-code-bin slack-desktop nvm heroku-cli nerd-fonts-complete
+sudo yaourt -S google-chrome-dev visual-studio-code-bin slack-desktop nvm heroku-cli nerd-fonts-complete postman-bin signal-desktop-bin tmuxinator ulauncher
